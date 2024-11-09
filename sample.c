2b8ce8915af86486211aea5117792157f58837d3
Yonatan Haddish <yonihailshn@gmail.com>
2024-11-09 02:03:18 +0000

    scan 002

--- sample.c
+++ sample.c
@@ -1,17 +1,42 @@
-#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
- 
+#include <sys/stat.h>
+
 #define BUFSIZE 256
-    
+
 // This program prints the size of a specified file in bytes
 int main(int argc, char** argv) {
     // Ensure that the user supplied exactly one command line argument
-    if (argc != 2) { 
+    if (argc != 2) {
         fprintf(stderr, "Please provide the address of a file as an input.\n");
         return -1;
     }
-    char cmd[BUFSIZE] = "wc -c < ";
-    strcat(cmd, argv[1]);
-    system(cmd);
+
+    struct stat fileStat;
+    // Use stat to get file information
+    if (stat(argv[1], &fileStat) == -1) {
+        perror("Error getting file stats");
+        return -1;
+    }
+
+    // Print the file size in bytes
+    printf("File size: %ld bytes\n", fileStat.st_size);
+    return 0;
 }
+
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
