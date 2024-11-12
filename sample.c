b44b1e04e032036f697af32c3366eb9598c0eee2
Yonatan Haddish <yonihailshn@gmail.com>
2024-11-12 22:50:34 +0000

    scan 003

--- sample.c
+++ sample.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
-    
-int main(int argc, char **argv) {
-    char *items[] = {"boat", "car", "truck", "train"};
-    int index = atoi(argv[1]);
-    printf("You selected %s\n", items[index]);
-}
+int main() {
+	char last_name[20];
+	printf ("Enter your last name: ");
+	scanf ("%s", last_name);
+	printf("Entered last name is %s\n", last_name);
+	}
