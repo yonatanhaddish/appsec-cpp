0ddadea60664afbaf3077f0489b35be47a9fcdaf
Yonatan Haddish <yonihailshn@gmail.com>
2024-11-12 23:22:55 +0000

    scan 004

--- sample.c
+++ sample.c
@@ -1,5 +1,8 @@
-int main (int argc, char **argv) {
-char *items[] = {"boat", "car", "truck", "train"};
-int index = GetUntrustedOffset();
-printf("You selected %s\n", items[index-1]);
+#include <stdio.h>
+#include <stdlib.h>
+    
+int main(int argc, char **argv) {
+    char *items[] = {"boat", "car", "truck", "train"};
+    int index = atoi(argv[1]);
+    printf("You selected %s\n", items[index-1]);
 }
