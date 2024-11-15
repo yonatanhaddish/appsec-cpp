9742e151e9a22d5e07d3cfc01607d111bfecf348
Yonatan Haddish <yonihailshn@gmail.com>
2024-11-15 14:49:27 +0000

    tesdt02

--- sample.c
+++ sample.c
@@ -4,5 +4,5 @@
 int main(int argc, char **argv) {
     char *items[] = {"boat", "car", "truck", "train"};
     int index = atoi(argv[1]);
-    printf("You selected %s\n", items[index-1]);
+    printf("You selected %s\n", items[index]);
 }
